06f2c7c8c37606695e18cc7372537acc62c0a807
Cristian D. Moreno - Kyonax <kyonax25@gmail.com>
2025-06-11 14:59:04 -0500

    Refining the First Section
    Modified-by: Cristian D. Moreno - Kyonax <kyonax25@gmail.com>

--- src/index.tsx
+++ src/index.tsx
@@ -12,27 +12,26 @@ export function App({ data }) {
 		<div id="root">
 			<section class="loader">
 				<div class="loader__container">
+					<p>
+						<span>Aparta la Fecha</span>
+						<br />
+						27. 02 .2026
+					</p>
 					<Handwritten
 						text={data?.text || FALLBACK_TEXT}
 						fontSize={100}
 						letterDelay={0.22}
 					/>
-					<p>
-						Hemos Reservado {data?.reserved || FALLBACK_RESERVED} lugar(es) en
-						su honor
-					</p>
+					<p>{data?.reserved || FALLBACK_RESERVED} lugar(es) en Reserva</p>
+				</div>
+			</section>
+			<section class="main">
+				<div class="main__seal">
+					{/* <span>ABRE LA INVITACIÓN</span> &nbsp;
+					<span>ABRE LA INVITACIÓN</span> */}
 				</div>
 			</section>
 		</div>
-	);
-}
-
-function Resource(props) {
-	return (
-		<a href={props.href} target="_blank" class="resource">
-			<h2>{props.title}</h2>
-			<p>{props.description}</p>
-		</a>
 	);
 }
 
