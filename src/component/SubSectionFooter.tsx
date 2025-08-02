42b4c3ea9811f40f9bb70f732b3b9d10fc615343
Cristian D. Moreno - Kyonax <kyonax25@gmail.com>
2025-08-02 15:58:31 -0500

    Adding Footer Info
    Modified-by: Cristian D. Moreno - Kyonax <kyonax25@gmail.com>

---
+++ src/component/SubSectionFooter.tsx
@@ -0,0 +1,20 @@
+import "./scss/SubSectionFooter.scss";
+
+const SubSectionFooter = () => (
+  <footer class="footer-section">
+    <div class="section-container">
+      <p>
+        Hecho con &nbsp;♥&nbsp; por{" "}
+        <a
+          href="https://kyonax.github.io/?language=es"
+          target="_blank"
+          rel="noopener"
+        >
+          @kyonax_on_tech
+        </a>
+      </p>
+    </div>
+  </footer>
+);
+
+export default SubSectionFooter;
